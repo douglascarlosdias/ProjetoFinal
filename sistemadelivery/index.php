39f37301bcdcd47bb19c220d2c5ddeec4e91ccab
douglascarlosdias <douglasdcdnet@gmail.com>
2023-09-12 19:36:03 -0300

    link css

--- sistemadelivery/index.php
+++ sistemadelivery/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pizzaria delivery</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
 <img src="" alt="">
