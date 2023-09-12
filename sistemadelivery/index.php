5288c0e5dd2587e02af6a92bec65364a582c2a60
douglascarlosdias <douglasdcdnet@gmail.com>
2023-09-12 18:40:37 -0300

    inicio do cabeçalho

--- sistemadelivery/index.php
+++ sistemadelivery/index.php
@@ -3,30 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NOSSO PROJETO FINAL</title>
+    <title>Pizzaria delivery</title>
 </head>
 <body>
-    <h1>ESSE É O NOSSO PROJETO PESSOAL </h1>
-    <h2> projeto tcc </h2>
-    <h2>meu segundo commit</h2>
-    <h3>alterado no vs code</h3>
-    <h4>alterado no git hub</h4>
-    <fieldset>
-    <form action="" method="post">
-        <p>nome: <input type="text" name="nome"></p>
-        <p>email: <input type="email" name="email" id=""></p>
-        <p><input type="submit" value="enviar"></p>
-    </form>
-    </fieldset>
-    <table>
-        <tr>
-            <td>carro</td>
-            <td>moto</td>
-            <td>caminhão</td>
-        </tr>
-    </table>
-    <p>alteracao feita no git</p>
-    <p>inclusao feita no vs code </p>
-    
+    <header>
+        <nav>
+            <ul>
+                <li> <a href="#">Cardápio</a></li>
+                <li>promoções</li>
+                <li>localização</li>
+                <li>login</li>
+            </ul>
+        </nav>
+    </header>
+   
 </body>
 </html>
