23c178fc3515327d99a2e61469cf948e7e95699b
douglascarlosdias <douglasdcdnet@gmail.com>
2023-10-09 21:11:06 -0300

    CABEÇALHO E CSS

--- sistemadelivery/index.php
+++ sistemadelivery/index.php
@@ -6,10 +6,25 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
   <title>home</title>
 </head>
 <body>
-
+  <header class="main_header">
+    <div class="main_header_content">
+      <a href=""> <img src="img/transferir.jpeg" alt=""> </a>
+       <nav class="main_header_content_menu">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Promoções </a></li>
+                <li><a href="">Cardápio</a></li>
+                <li><a href="cadastro.php">Cadastro</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="">Carrinho</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
 
 
 
---
+++ sistemadelivery/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
+    <title>login</title>
+</head>
+<body>
+    <div class="login_content">
+        <form action="" method="post">
+        <fieldset>
+            <legend>login</legend>
+            <p>Email:<input type="email" name="email" id="" required placeholder="email"></p>
+            <p>Senha:<input type="password" name="senha" id="" required placeholder="senha"></p>
+            <button>Entrar</button><button><a href="cadastro.php" >Cadastrar</a> </button><button><a href="index.php">home</a></button>
+        </fieldset>
+    </div>
+</form>
+
+    
+</body>
+</html>
