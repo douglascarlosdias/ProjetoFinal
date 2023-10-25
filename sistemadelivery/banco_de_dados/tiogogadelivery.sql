b0a23a12f3451c1a3941bc557bf9561a3e9c2845
douglascarlosdias <douglasdcdnet@gmail.com>
2023-10-24 23:35:57 -0300

    atualização do banco de dados atualizado!!

--- sistemadelivery/banco_de_dados/tiogogadelivery.sql
+++ sistemadelivery/banco_de_dados/tiogogadelivery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/10/2023 às 16:07
+-- Tempo de geração: 25/10/2023 às 04:33
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -31,11 +31,24 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nome` varchar(60) NOT NULL,
   `cpf` varchar(20) NOT NULL,
-  `endereço` varchar(80) NOT NULL,
+  `endereco` varchar(80) NOT NULL,
   `telefone` varchar(20) NOT NULL,
   `email` varchar(70) NOT NULL,
-  `senha` varchar(35) NOT NULL
+  `senha` varchar(35) NOT NULL,
+  `nivel` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `endereco`, `telefone`, `email`, `senha`, `nivel`) VALUES
+(3, 'maria ', '259.746.371-00', 'qr 415 conjunto 11 casa 24', '(61) 99406-5329', 'maria@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'cliente'),
+(4, 'maria ', '259.746.371-00', 'qr 415 conjunto 11 casa 24', '(61) 99406-5329', 'maria@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'cliente'),
+(5, 'monica ', '000.555.999-99', 'shsn avendia central', '(61) 99888-8888', 'monica@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'cliente'),
+(7, 'maria risoleta ', '024.230.851-17', 'Aguas lindas-Go', '(61) 99342-6065', 'mariarisoleta3@gmail.com', '64122b3d53eee5c95a9ad503ea0e9e72', 'admin'),
+(9, 'douglas', '012.398.621-48', 'qr 415 conjunto 11 casa 24', '(61) 99190-6531', 'douglasdcdnet@gmail.com', '5a2939e9a8645471c17c8b1d1ea161e6', 'admin'),
+(11, 'douglas', '259.746.371-00', 'qr 415 conjunto 11 casa 24', '(61) 99406-5329', 'dcdnet@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'cliente');
 
 --
 -- Índices para tabelas despejadas
@@ -55,7 +68,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
